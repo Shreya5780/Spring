e216316abea7e3cde9b9938a5af81a2fa1b78349
Shreya5780 <shreyachauhan1226@gmail.com>
2025-06-25 16:09:38 +0530

    Questions CRUD oprations done.

--- src/main/java/com/controller/QuestionController.java
+++ src/main/java/com/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.controller;
 import com.mongo.model.QuestionModel;
 import com.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,16 @@ public class QuestionController {
     @PostMapping("/add/{subjectId}")
     public QuestionModel addQuestion(@RequestBody QuestionModel question, @PathVariable String subjectId){
         return questionService.addQuestionsBySubject(subjectId,question);
+    }
+
+    @PutMapping("/update/{questionId}")
+    public QuestionModel updateQuestion(@RequestBody QuestionModel question, @PathVariable String questionId){
+        return questionService.updateQuestionsBySubject(questionId,question);
+    }
+
+    @DeleteMapping("/delete/{questionId}")
+    public ResponseEntity<String> deleteQuestion(@PathVariable String questionId){
+        return questionService.deleteSubject(questionId);
     }
 
 }
--- src/main/java/com/service/QuestionService.java
+++ src/main/java/com/service/QuestionService.java
@@ -1,11 +1,17 @@
 package com.service;
 
 import com.mongo.model.QuestionModel;
+import com.mongo.model.SubjectModel;
 import com.repository.QuestionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class QuestionService {
@@ -24,7 +30,7 @@ public class QuestionService {
         question.setQid(null);
         question.setSubjectId(subjectId);
         question.setQuestion(questionInfo.getQuestion());
-        question.setAnswer(questionInfo.getAnswer());
+        question.setAnswer(questionInfo.getAnswer().toUpperCase());
         question.setOption1(questionInfo.getOption1());
         question.setOption2(questionInfo.getOption2());
         question.setOption3(questionInfo.getOption3());
@@ -34,6 +40,60 @@ public class QuestionService {
 
         return questionInfo;
 
+    }
+
+    public QuestionModel updateQuestionsBySubject(String questionId, QuestionModel questionInfo){
+        QuestionModel question = questionRepo.findById(questionId).orElse(null);
+
+        if(question == null){
+            System.out.println("Question not found.");
+            return questionInfo;
+        }
+
+        /**
+         * this is slower
+         * so you can use this alternate also using if but this is difficult for large no. of fields
+         */
+//        Field[] fields = question.getClass().getDeclaredFields();
+//        for(Field field : fields){
+//            field.setAccessible(true);
+//            System.out.println(field.getName());
+//
+//            try {
+//               Object newValue = field.get(questionInfo);
+//               if(newValue != null){
+//                   field.set(question, newValue);
+//                   System.out.println(newValue + " " + field.getName());
+//               }
+//               else{
+////                   field.set(question, field.get(question));
+//                   System.out.println(field.getName() + " " + field.get(question));
+//               }
+//            } catch (IllegalAccessException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+
+       if(questionInfo.getQuestion() != null)  question.setQuestion(questionInfo.getQuestion());
+       if(questionInfo.getAnswer() != null) question.setAnswer(questionInfo.getAnswer());
+       if(questionInfo.getOption1() != null) question.setOption1(questionInfo.getOption1());
+       if(questionInfo.getOption2() != null) question.setOption2(questionInfo.getOption2());
+       if(questionInfo.getOption3() != null) question.setOption3(questionInfo.getOption3());
+       if(questionInfo.getOption4() != null) question.setOption4(questionInfo.getOption4());
+
+       questionRepo.save(question);
+
+        return question;
+
+    }
+
+    public ResponseEntity<String> deleteSubject(@PathVariable String questionId) {
+        QuestionModel questionModel = questionRepo.findById(questionId).orElse(null);
+        if (questionModel == null) {
+            return ResponseEntity.notFound().build();
+        }
+        questionRepo.delete(questionModel);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Successfully deleted successfully");
     }
 
 
